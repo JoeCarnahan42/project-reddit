f9aac566a0c8b16bff17fbe73ad46a14c38533fc
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2024-10-31 17:08:16 -0500

    Add structure to page and simple styling. Write notes on main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// create variables that hold element creation //
+// create variables that hold existing element data //
+// create a variable that has string info ("posted by:") //
+// create event listener that listens for clicks to add posts //
+// Make both text entries required for submission //
+
+
+// Phase Two: Comments //
+// Make posts into clickable buttons that open comment posting interface //
+// Create ability to remove comments //
+// More..?? //
