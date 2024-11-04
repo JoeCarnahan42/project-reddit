fe6e689c9f441ae4df878646b5dbfa0b63f8382c
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2024-11-04 13:35:58 -0600

    Begin work on comments section

--- main.js
+++ main.js
@@ -1,52 +1,30 @@
-
-// create variables that hold existing element data //
-// create a variable that has string info ("posted by:") //
-// create event listener that listens for clicks to add posts //
-// Make both text entries required for submission //
-
-
-// Phase Two: Comments //
-// Make posts into clickable buttons that open comment posting interface //
-// Create ability to remove comments //
-// More..?? //
-
-
-var postBtn = document.getElementById('post-btn')
-var postSection = document.getElementById('post-section')
-var postNameInput = document.getElementById('name-input')
-var postInput = document.getElementById('post-input')
-var removeBtn = document.getElementsByClassName('remove')[0]
-var commentsBtn = document.getElementsByClassName('comments')[0]
-var postList = document.getElementById('post-list')
-
+const postBtn = document.getElementById('post-btn')
+const postSection = document.getElementById('post-section')
+const postNameInput = document.getElementById('name-input')
+const postInput = document.getElementById('post-input')
+const removeBtn = document.getElementsByClassName('remove')[0]
+const commentsBtn = document.getElementsByClassName('comments')[0]
+const postList = document.getElementById('post-list')
+const commentInput = document.getElementById('comment-input')
+const commentName = document.getElementById('comment-name')
 
 postList.addEventListener('click', function(e) {
   if (e.target.classList.contains('remove')) {
     e.target.closest('.list-group-item').remove()
   } else if (e.target.classList.contains('comments')) {
     
+    
   }
 })
-
-
-
-
 
 // Handles posting //
 postBtn.addEventListener('click', function () {
   if (!postNameInput.value || !postInput.value || postNameInput.value === " " || postInput.value === " ") {
     alert('Both fields must be filled out to post.')
   }else {
-    postSection.innerHTML += `<li class="list-group-item"><button type="button" class="btn btn-info btn-md remove">Remove</button> <button type="button" class="btn btn-info btn-md comments" >Comments</button> ${postInput.value} - Posted By: ${postNameInput.value} </li>`
-  
+    postSection.innerHTML += `<h5 class="list-group-item"><button type="button" class="btn btn-info btn-md remove">Remove</button> <button type="button" class="btn btn-info btn-md comments" >Comments</button> ${postInput.value} - Posted By: ${postNameInput.value} </h5>`
+    postSection.innerHTML += '<div  class="input-group input-group-lg mb-3" ><input type="text" class="form-control" placeholder="Comment" /></div>'
     postNameInput.value = ""
     postInput.value = ""
-
   }
-  
-
 })
-
-
-  
-
