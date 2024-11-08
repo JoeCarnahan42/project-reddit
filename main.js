642f55857ff599133111b516483918b3663007a4
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2024-11-08 14:37:05 -0600

    Refactor code to add comment input fields upon button click. Assign ID's to each post.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const allPosts = {}
+let data = []
 const postBtn = document.getElementById('post-btn')
 const postSection = document.getElementById('post-section')
 const postNameInput = document.getElementById('name-input')
@@ -6,34 +6,74 @@ const postInput = document.getElementById('post-input')
 const removeBtn = document.getElementsByClassName('remove')[0]
 const commentsBtn = document.getElementsByClassName('comments')[0]
 const postList = document.getElementById('post-list')
-const commentInput = document.getElementById('comment-input')
-const commentName = document.getElementById('comment-name')
 
 postList.addEventListener('click', function(e) {
   if (e.target.classList.contains('remove')) {
     e.target.closest('.list-group-item').remove()
   } else if (e.target.classList.contains('comments')) {
-    renderComments()
+    console.log(e.target.closest('div'))
+    commentsInputFields(e)
+    // renderComments()
     
-    // `<div class="input-group input-group-lg mb-3"><input type="text" class="form-control" placeholer="Comment Text"/></div>`
   }
 })
 
+function commentsInputFields(e) {
+  const nearestDiv = e.target.closest('div')
+  const div1 = document.createElement('div')
+  div1.className = 'container'
+
+  const div2 = document.createElement('div')
+  div2.className = 'container'
+
+  const inputDiv1 = document.createElement('div')
+  inputDiv1.className = 'input-group input-group-lg mb-3'
+
+  const inputDiv2 = document.createElement('div')
+  inputDiv2.className = "input-group input-group-lg mb-3"
+
+  const commentsInput = document.createElement('input')
+  commentsInput.type = 'text'
+  commentsInput.className = 'form-control'
+  commentsInput.placeholder = 'Comment Text'
+  
+  const commentsNameInput = document.createElement('input')
+  commentsNameInput.type = 'text'
+  commentsNameInput.className = 'form-control'
+  commentsNameInput.placeholder = 'Your Name'
+  
+  nearestDiv.append(div1)
+  nearestDiv.append(div2)
+  div1.append(inputDiv1)
+  div2.append(inputDiv2)
+  inputDiv1.append(commentsInput)
+  inputDiv2.append(commentsNameInput)
+}
+
 function renderComments() {
-  allPosts.comments.forEach((comment) => {
-    postSection.innerHTML += `<div class="list-group-item"><h6>${comment}</h6></div>`
-  })
+
+    
+  
 }
 
 function renderPosts() {
   if (!postNameInput.value || !postInput.value || postNameInput.value === " " || postInput.value === " ") {
     alert('Both fields must be filled out to post.')
   }else {
-    allPosts[` ${postInput.value} -Posted By: ${postNameInput.value}`] = {comments: []}
-    postSection.innerHTML = ""
-    for (posts in allPosts) {
-      postSection.innerHTML += `<div class="list-group-item"><h5><button type="button" class="btn btn-info btn-md remove">Remove</button> <button type="button" class="btn btn-info btn-md comments" >Comments</button> ${posts} </h5></div>`
+    const randomId = Math.random()
+    let post = {
+      id: randomId,
+      posterName: postNameInput.value,
+      postText: postInput.value,
+      comments: []
     }
+    data.push(post)
+    console.log(data)
+    postSection.innerHTML = ""
+    data.forEach((post) => {
+      postSection.innerHTML += `<div class="list-group-item"><h5><button type="button" class="btn btn-info btn-md remove">Remove</button> <button type="button" id="${randomId}" class="btn btn-info btn-md comments" >Comments</button> ${post.postText} -Posted By: ${post.posterName} </h5></div>`
+    })
+    
     postNameInput.value = ""
     postInput.value = ""
   }
@@ -42,5 +82,11 @@ function renderPosts() {
 // Handles posting //
 postBtn.addEventListener('click', function () {
   renderPosts()
-  console.log(allPosts)
 })
+
+
+// `<div class="input-group input-group-lg mb-3"><input type="text" class="form-control" placeholer="Comment Text"/></div>`
+
+// let div = document.createElement("div");
+// let p = document.createElement("p");
+// div.append(p);
