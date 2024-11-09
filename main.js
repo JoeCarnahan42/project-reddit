11b6089a7cf1802584eba28c30d04fb928e580b6
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2024-11-08 22:00:55 -0600

    Add 'add comment' button. Fix remove button to remove the specific post from html and the array.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let data = []
+const data = []
 const postBtn = document.getElementById('post-btn')
 const postSection = document.getElementById('post-section')
 const postNameInput = document.getElementById('name-input')
@@ -9,71 +9,93 @@ const postList = document.getElementById('post-list')
 
 postList.addEventListener('click', function(e) {
   if (e.target.classList.contains('remove')) {
+    const targetId = JSON.parse(e.target.id)
+    const indexToRem = data.findIndex(post => post.remId === targetId)
+    data.shift(indexToRem)
     e.target.closest('.list-group-item').remove()
   } else if (e.target.classList.contains('comments')) {
-    console.log(e.target.closest('div'))
+
+    // Add and if else that checks if the elements already exist
     commentsInputFields(e)
     // renderComments()
     
+  } else if (e.target.classList.contains('btn-primary')) {
+    const commentData = {
+      name: commentsNameInput.value,
+      comment: commentsInput.value
+    }
+
+    // Find a way to link the button to the id and thus the index and push it in to the comments array //
+    
+    const postId = e.target.closest('.comments')
+    console.log(postId)
   }
 })
 
+const div1 = document.createElement('div')
+const div2 = document.createElement('div')
+const inputDiv1 = document.createElement('div')
+const inputDiv2 = document.createElement('div')
+const commentsInput = document.createElement('input')
+const commentsNameInput = document.createElement('input')
+
 function commentsInputFields(e) {
+  const addCommentBtn = document.createElement('button')
+  addCommentBtn.type = 'button'
+  addCommentBtn.className = 'btn btn-primary'
+  addCommentBtn.innerText = 'Post Comment'
   const nearestDiv = e.target.closest('div')
-  const div1 = document.createElement('div')
   div1.className = 'container'
 
-  const div2 = document.createElement('div')
   div2.className = 'container'
 
-  const inputDiv1 = document.createElement('div')
   inputDiv1.className = 'input-group input-group-lg mb-3'
 
-  const inputDiv2 = document.createElement('div')
   inputDiv2.className = "input-group input-group-lg mb-3"
 
-  const commentsInput = document.createElement('input')
   commentsInput.type = 'text'
   commentsInput.className = 'form-control'
   commentsInput.placeholder = 'Comment Text'
   
-  const commentsNameInput = document.createElement('input')
   commentsNameInput.type = 'text'
   commentsNameInput.className = 'form-control'
   commentsNameInput.placeholder = 'Your Name'
   
   nearestDiv.append(div1)
   nearestDiv.append(div2)
+  nearestDiv.append(addCommentBtn)
   div1.append(inputDiv1)
   div2.append(inputDiv2)
   inputDiv1.append(commentsInput)
   inputDiv2.append(commentsNameInput)
 }
 
-function renderComments() {
+function renderComments(e) {
 
-    
-  
+  const post = e.target.closest('h5')
+  const commentSection = document.createElement('div')
+  commentSection.className = 'list-group-item'
+  post.append(commentSection)
 }
 
 function renderPosts() {
   if (!postNameInput.value || !postInput.value || postNameInput.value === " " || postInput.value === " ") {
     alert('Both fields must be filled out to post.')
   }else {
-    const randomId = Math.random()
-    let post = {
-      id: randomId,
+    const randomComId = Math.random()
+    const randomRemId = Math.floor(Math.random() * 500)
+    const post = {
+      remId: randomRemId,
+      id: randomComId,
       posterName: postNameInput.value,
       postText: postInput.value,
       comments: []
     }
     data.push(post)
-    console.log(data)
     postSection.innerHTML = ""
     data.forEach((post) => {
-      postSection.innerHTML += `<div class="list-group-item"><h5><button type="button" class="btn btn-info btn-md remove">Remove</button> <button type="button" id="${randomId}" class="btn btn-info btn-md comments" >Comments</button> ${post.postText} -Posted By: ${post.posterName} </h5></div>`
+      postSection.innerHTML += `<div class="list-group-item"><h5><button id="${randomRemId}" type="button" class="btn btn-info btn-md remove">Remove</button> <button type="button" id="${randomComId}" class="btn btn-info btn-md comments" >Comments</button> ${post.postText} -Posted By: ${post.posterName} </h5></div>`
     })
-    
     postNameInput.value = ""
     postInput.value = ""
   }
