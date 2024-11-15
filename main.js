e619371261f7a530f326643119808d291bf8ccb3
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2024-11-15 15:47:14 -0600

    Begin work on commenting system, refactor the way posts are handled

--- main.js
+++ main.js
@@ -9,69 +9,61 @@ const commentSection = document.createElement('div')
 const comments = document.createElement('h5')
 comments.classList.add('d-none') // Hides comments until rendered //
 const postBtn = document.getElementById('post-btn')
+const postContent = document.createElement('h5')
+const lineBreak = document.createElement('br')
 
 
 function makePost() {
   const postInput = document.getElementById('post-input')
   const posterName = document.getElementById('name-input')
   const postId = Math.random(Math.floor * 1000)
-  console.log(postId)
+  const postData = `
+    <button class='btn btn-info btn-md remove' type='button' id='${postId}'>Remove</button> 
+    <button class='btn btn-info btn-md comment' type='button' id='${postId}'>Comment</button> 
+    ${postInput.value} - Posted By: ${posterName.value}`
   const post = {
     id: postId,
-    postText: postInput.value,
-    posterName: posterName.value,
+    postText: postData,
     comments: []
   }
   posts.push(post)
-  postList.innerHTML = ''
+  postContent.innerHTML = ''
   posts.forEach((post) => {
     // Creates post elements and renders post/content //
-    const postContent = document.createElement('h5')
     postList.appendChild(postDiv)
     postDiv.appendChild(postContent)
-    postContent.innerHTML = `
-      <button class='btn btn-info btn-md remove' type='button' id='${postId}'>Remove</button> 
-      <button class='btn btn-info btn-md comment' type='button' id='${postId}'>Comment</button> 
-      ${post.postText} - Posted By: ${post.posterName}`
-      postInput.value = ''
-      posterName.value = ''
-    // Comments //
-    commTextInput.className = 'input-group input-group-md mb-3 form-control d-none'
-    commTextInput.id = 'comm-text-input'
+    postContent.appendChild(lineBreak)
+    postContent.innerHTML += post.postText
+    // Comments Section Construction //
+    commTextInput.className = 'input-group input-group-md mb-3 form-control comm-text-input d-none'
+    commTextInput.id = JSON.stringify(postId)
     commTextInput.placeholder = 'Comment Text'
-    commNameInput.className = 'input-group input-group-md mb-3 form-control d-none'
-    commNameInput.id = 'comm-name-input'
+    commNameInput.className = 'input-group input-group-md mb-3 form-control comm-name-input d-none'
+    commNameInput.id = JSON.stringify(postId)
     commNameInput.placeholder = 'Your Name'
     commPostBtn.className = 'btn btn-primary post-comm d-none'
     commPostBtn.type = 'button'
     commPostBtn.innerText = 'Post Comment'
     commPostBtn.id = postId
-    post.comments.forEach((comment) => {
-      postDiv.appendChild(commentSection)
-      commentSection.appendChild(comments)
-      comments.innerHTML = `
-        <button type='button' class='btn btn-info btn-sm remove' id='${comment.commentId}'>Remove</button>
-         ${comment.commentText} - Posted By: ${comment.commenterName}`
-      
-    })
     // Add comment inputs //
     postDiv.appendChild(commTextInput)
     postDiv.appendChild(commNameInput)
     postDiv.appendChild(commPostBtn)
   })
+  postInput.value = ''
+  posterName.value = ''
 }
 
 function commentsRenderToggle() {
+  // Link post id and render comments from post //
   console.log(comments.classList)
   console.log(comments.classList.contains('d-none'))
   if (comments.classList.contains('d-none')) {
-    console.log('it was hidden')
     comments.classList.remove('d-none')
     commTextInput.classList.remove('d-none')
     commNameInput.classList.remove('d-none')
     commPostBtn.classList.remove('d-none')
   } else {
-    console.log('it was not hidden')
     comments.classList.add('d-none')
     commTextInput.classList.add('d-none')
     commNameInput.classList.add('d-none')
@@ -81,18 +73,29 @@ function commentsRenderToggle() {
 
 function postComment(e) {
   const commId = Math.random()
-  const commTextInput = document.getElementById('comm-text-input')
-  const commNameInput = document.getElementById('comm-name-input')
+  const commTextInput = document.querySelector('.comm-text-input')
+  const commNameInput = document.querySelector('.comm-name-input')
+  const commText = commTextInput.value
+  const commName = commNameInput.value
+  const commData = `
+  <button type='button' class='btn btn-info btn-sm remove' id='${commId}'>Remove</button>
+  ${commText} - Posted By: ${commName}`
+  
   const commentData = {
     commentId: commId,
-    commentText: commTextInput.value,
-    commenterName: commNameInput.value
+    commentText: commData,
   }
   const postIndex = posts.findIndex(post => post.id === JSON.parse(e.target.id))
   posts[postIndex].comments.push(commentData)
-  
-  // Pushes comment to post with correct id //
-  // Renders comment to comment section above comment inputs (findIndex) //
+  comments.innerHTML = ''
+  posts[postIndex].comments.forEach((comment) => {
+    postContent.appendChild(commentSection)
+    commentSection.appendChild(comments)
+    comments.appendChild(lineBreak)
+    comments.innerHTML += comment.commentText
+  })
+  commTextInput.value = ''
+  commNameInput.value = ''
 }
 
 postBtn.addEventListener('click', function() {
@@ -107,24 +110,13 @@ postList.addEventListener('click', function(e) {
   }
 })
 
+// !!Problems!! //
+// 1. Once a new post is made, the comments are unrendered, but still exist in the array.
+// 2. Clicking the comments button does not render the ONLY the comments under that particular post.
+// 3. Once a comment is posted, it will render under any post that the comments button is clicked.
+// 4. Probably more that i have no found yet...
 
-function renderNewComm() {
-  // appends a newly added comment to the correct postId //
-}
-
-
-
-// const posts = [
-//   {id: 1,
-//    postText: 'sample post 1',
-//    posterName: 'jojo',
-//    comments: []},
-//   {id: 2,
-//    postText: 'sample post 2',
-//    posterName: 'jiji',
-//    comments: []},
-//   {id: 3,
-//    postText: 'sample post 3',
-//    posterName: 'dodo',
-//    comments: []}
-// ]
+// !! TODO !! //
+// 1. Code remove buttons on posts(easy)
+// 2. Code remove buttons on comments(probably easy)
+// 3. Fix errors...
